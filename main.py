65f712bd81fe0d67cb003f48bc73ab6cc19dfcef
moltenangel <matthew@gunur.net>
2023-10-20 23:45:00 -0400

    First test

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python3
 
+from gpiozero import LED
+from time import sleep
+
+led = LED(24)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
+
+red.blink()
 
 print("Hello World!")
+
+print("Round One, Fight!")
