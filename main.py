5ab463935e36c64ce6fb5e143ca0256f7e739508
moltenangel <matthew@gunur.net>
2023-11-03 22:24:01 -0400

    Added/Updated files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 3-Color LED controlled by a rotary encoder with a Raspberry Pi Model 2 B
 # written by Matthew Greene
-# 11/2/23
+# 11/3/23
 # References:
 # Rotary Encoder: https://github.com/gpiozero/gpiozero/blob/master/gpiozero/input_devices.py#L982
 
@@ -14,20 +14,41 @@ import random as rdm
 
 # Initialize GPIO Board values
 totalsteps = 90  # default = 360
-# led = RGBLED(red=17, green=27, blue=22, active_high=False)
-#rotor = RotaryEncoder(24, 18, wrap=True, maxsteps=totalsteps/2)
-rotor = RotaryEncoder(24, 18, wrap=True,)  #  maxsteps=totalsteps/2
 rotor.steps = -(totalsteps/2)  # -180
-btn = Button(23)  # btn = Button(23, pull_up=False)
+rotor = RotaryEncoder(24, 18, wrap=True)  #  maxsteps=totalsteps/2
+led = RGBLED(red=17, green=27, blue=22, active_high=False)
+btn = Button(23)  # pull_up=False)
 done = Event()
 
 def main():
-    print("Hello World!")
+    print("Raspberry Pi device")
     print("Round One, Fight!")
     speed = .03
     maxspeed = 50
+    mode = 0
+    hue = (rotor.steps + totalsteps / 2) / totalsteps
 
-    def blink():
+    def change_mode():
+        global totalsteps
+        nonlocal modenon
+        local hue
+
+        mode += 1
+        print(f'mode={mode}')
+
+        if mode == 1:
+            print('blink')
+            led.blink(on_time=1, off_time=1, fade_in_time=0, fade_out_time=0, on_color=Color(h=hue, s=1, v=1), off_color = (0, 0, 0), n=None, background=True)
+        elif mode == 2:
+            print('pulse')
+            led.pulse(fade_in_time=1, fade_out_time=0, on_color=Color(h=hue, s=1, v=1), off_color=(0, 0, 0), n=None, background=True)
+        else:
+            print('mode=0')
+            mode = 0
+            led.on()
+            led.color = Color(h=hue, s=1, v=1)
+
+    def rdm_color():
         red = rdm.randrange(0, totalsteps, 1) / totalsteps
         green = rdm.randrange(0, totalsteps, 1) / totalsteps
         blue = rdm.randrange(0, totalsteps, 1) / totalsteps
@@ -41,25 +62,33 @@ def main():
 
         def change_hue():
             # Scale the rotor steps (-180..180) to 0..1
+            print(f'rotor.steps={rotor.steps}' | print(f,'totalsteps={totalsteps}'))
+            nonlocal hue
             hue = (rotor.steps + totalsteps/2) / totalsteps
-            led.color = Color(h=hue, s=1, v=1)
+            if led.is_lit == True:
+                led.color = Color(h=hue, s=1, v=1)
 
         def show_color():
             print(f'Hue {led.color.hue.deg:.1f} degrees = {led.color.html}')
-            blink()
+#            change_mode()
 
         def stop_script():
             print('Exiting')
             done.set()
 
         print('Select a color by turning the knob')
-        rotor.when_rotated = change_hue
         print('Select a color by turning the knob')
-        btn.when_released = show_color
         print('Hold the button to exit')
-        btn.when_held = stop_script
+        print('Goodbye...')
+
+        # Event Handler
+        rotor.when_rotated = change_hue
+        btn.when_pressed = show_color  # led_toggle
+        btn.when_released = change_mode  # show_color
+        btn.when_held = led.toggle  # stop_script
         done.wait()
         print('Goodbye...')
 
 
 if __name__ == '__main__':
+    main()
